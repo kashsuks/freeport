0d502a8237b7602401ff20dd2324e3e4d0c3a9a2
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-12-19 16:46:46 -0500

    chore: wiring config handler

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	WelcomeMessage string `json:"welcome_message"`
+}
+
+func getConfig() string {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return ".freeport_config.json"
+	}
+
+	return filepath.Join(home, ".freeport_config.json")
+}
+
+func Load() *Config {
+	cfg := &Config{
+		WelcomeMessage: "Welcome to Freeport!",
+	}
+
+	data, err := os.ReadFile(getConfig())
+	if err != nil {
+		return cfg
+	}
+
+	json.Unmarshal(data, cfg)
+	return cfg
+}
+
+func (c *Config) Save() error {
+	data, err := json.MarshalIndent(c, "", " ")
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(getConfig(), data, 0644)
+}
